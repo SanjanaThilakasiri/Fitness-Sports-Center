dfdb42b226970d7162caf65b89e8428c4b3394be
SanjanaThilakasiri <160069278+SanjanaThilakasiri@users.noreply.github.com>
2026-03-19 14:56:18 +0530

    add trainers section

--- src/sections/Trainers.jsx
+++ src/sections/Trainers.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import trainer1 from "../assets/trainer1.jpg";
+import trainer2 from "../assets/trainer2.jpg";
+import trainer3 from "../assets/trainer3.jpg";
+import trainer4 from "../assets/trainer4.jpg";
 
-const Trainers = () => {
+const trainers = [
+  {
+    name: "Kasun",
+    role: "Strength Coach",
+    image: trainer1,
+  },
+  {
+    name: "Saduni",
+    role: "Personal Trainer",
+    image: trainer2,
+  },
+  {
+    name: "Ruwan",
+    role: "Fitness Instructor",
+    image: trainer3,
+  },
+  {
+    name: "Amali",
+    role: "Yoga Instructor",
+    image: trainer4,
+  },
+];
+
+export default function Trainers() {
   return (
-    <div></div>
-  )
-}
+    <section className="trainers" id="trainers">
+      <div className="trainers-container">
 
-export default Trainers
+        <h2 className="trainers-heading">Meet Our Trainers</h2>
+
+        <div className="trainers-grid">
+          {trainers.map((trainer) => (
+            <div className="trainer-card" key={trainer.name}>
+
+              {/* Photo */}
+              <div className="trainer-img-wrapper">
+                <img
+                  src={trainer.image}
+                  alt={trainer.name}
+                  className="trainer-img"
+                />
+              </div>
+
+              {/* Info */}
+              <div className="trainer-info">
+                <h3 className="trainer-name">{trainer.name}</h3>
+                <p className="trainer-role">{trainer.role}</p>
+              </div>
+
+            </div>
+          ))}
+        </div>
+
+      </div>
+    </section>
+  );
+}
